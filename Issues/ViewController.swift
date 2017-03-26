98cf5dad849dbf73f2c44dd4d91b875d495a6467
amlcurran <aml.curran@gmail.com>
2017-03-26 22:43:32 +0100

    Purge the storyboard and get correct certificate

--- Issues/ViewController.swift
+++ Issues/ViewController.swift
@@ -1,27 +1,15 @@
-//
-//  ViewController.swift
-//  Issues
-//
-//  Created by Alex Curran on 26/03/2017.
-//  Copyright (c) 2017 Alex Curran. All rights reserved.
-//
-
 import UIKit
-
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
 
     override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-
-
 
 }
